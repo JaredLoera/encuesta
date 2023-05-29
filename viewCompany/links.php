b963dba9fa312721f47fef8276ad9514c805b8c7
user <salazarloerajared@gmail.com>
2023-05-29 16:47:51 -0600

    sessions aging change

--- viewCompany/links.php
+++ viewCompany/links.php
@@ -1,7 +1,4 @@
-<?php 
-include '../clases/login.inc.php';
-login::sessionCompany();
-?>
+
 <a class="nav-link text-white" href="trabajadores.php">Trabajadores</a>
 <a class="nav-link text-white" href="encuestas.php">Encuestas</a>
 <a class="nav-link text-white" href="respuestasWorker.php">Encuestas realizadas</a>
