13417ffad1538e426f9583295bfeba680d585319
user <salazarloerajared@gmail.com>
2023-05-31 13:07:44 -0600

    delete link

--- viewRoot/companys.php
+++ viewRoot/companys.php
@@ -9,7 +9,6 @@
     <meta name="description" content="">
     <meta name="author" content="">
     <title>Panel empresas</title>
-    <link href="../assets/css/all.min.css" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
     <link href="../assets/css/sb-admin-2.min.css" rel="stylesheet">
     <link rel="stylesheet" href="../assets/css/bootstrap-reboot.css">
