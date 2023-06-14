e75994caf9dbd206e4d284c6c228224fbcfa2e8d
VictorIsaacLK <victor_almanzap@outlook.com>
2023-06-14 08:22:06 -0600

    reedit.sql

--- clases/database/reedit.sql
+++ clases/database/reedit.sql
@@ -136,15 +136,15 @@ DELIMITER $$
 DROP PROCEDURE IF EXISTS `preguntasCapitulo1` $$
 CREATE PROCEDURE `preguntasCapitulo1`(IN capitulo_id INT)
 BEGIN
-	INSERT INTO question (pregunta,capitulo_id,calsificacion) VALUE("Mi trabajo me exige hacer mucho esfuerzo físico",capitulo_id,1);/*1*/
-    INSERT INTO question (pregunta,capitulo_id,calsificacion) VALUE("Me preocupa sufrir un accidente en mi trabajo",capitulo_id,1);/*2*/
-    INSERT INTO question (pregunta,capitulo_id,calsificacion) VALUE("Considero que las actividades que realizo son peligrosas",capitulo_id,1);/*3*/
-    INSERT INTO question (pregunta,capitulo_id,calsificacion) VALUE("Por la cantidad de trabajo que tengo debo quedarme tiempo adicional a mi turno",capitulo_id,1);/*4*/
-    INSERT INTO question (pregunta,capitulo_id,calsificacion) VALUE("Por la cantidad de trabajo que tengo debo trabajar sin parar",capitulo_id,1);/*5*/
-    INSERT INTO question (pregunta,capitulo_id,calsificacion) VALUE("Considero que es necesario mantener un ritmo de trabajo acelerado",capitulo_id,1);/*6*/
-    INSERT INTO question (pregunta,capitulo_id,calsificacion) VALUE("Mi trabajo exige que esté muy concentrado",capitulo_id,1);/*7*/
-    INSERT INTO question (pregunta,capitulo_id,calsificacion) VALUE("Mi trabajo requiere que memorice mucha información",capitulo_id,1);/*8*/
-    INSERT INTO question (pregunta,capitulo_id,calsificacion) VALUE("Mi trabajo exige que atienda varios asuntos al mismo tiempo",capitulo_id,1);/*9*/
+	INSERT INTO question (pregunta,capitulo_id,calsificacion,item) VALUE("Mi trabajo me exige hacer mucho esfuerzo físico",capitulo_id,1,1);/*1*/
+    INSERT INTO question (pregunta,capitulo_id,calsificacion,item) VALUE("Me preocupa sufrir un accidente en mi trabajo",capitulo_id,1,2);/*2*/
+    INSERT INTO question (pregunta,capitulo_id,calsificacion,item) VALUE("Considero que las actividades que realizo son peligrosas",capitulo_id,1,3);/*3*/
+    INSERT INTO question (pregunta,capitulo_id,calsificacion,item) VALUE("Por la cantidad de trabajo que tengo debo quedarme tiempo adicional a mi turno",capitulo_id,1,4);/*4*/
+    INSERT INTO question (pregunta,capitulo_id,calsificacion,item) VALUE("Por la cantidad de trabajo que tengo debo trabajar sin parar",capitulo_id,1,5);/*5*/
+    INSERT INTO question (pregunta,capitulo_id,calsificacion,item) VALUE("Considero que es necesario mantener un ritmo de trabajo acelerado",capitulo_id,1,6);/*6*/
+    INSERT INTO question (pregunta,capitulo_id,calsificacion,item) VALUE("Mi trabajo exige que esté muy concentrado",capitulo_id,1,7);/*7*/
+    INSERT INTO question (pregunta,capitulo_id,calsificacion,item) VALUE("Mi trabajo requiere que memorice mucha información",capitulo_id,1,8);/*8*/
+    INSERT INTO question (pregunta,capitulo_id,calsificacion,item) VALUE("Mi trabajo exige que atienda varios asuntos al mismo tiempo",capitulo_id,1,9);/*9*/
 END$$
 DELIMITER ;
 
@@ -152,10 +152,10 @@ DELIMITER $$
 DROP PROCEDURE IF EXISTS `preguntasCapitulo2` $$
 CREATE PROCEDURE `preguntasCapitulo2`(IN capitulo_id INT)
 BEGIN
-    INSERT INTO question (pregunta,capitulo_id,calsificacion) VALUE("En mi trabajo soy responsable de cosas de mucho valor",capitulo_id,1);/*10*/
-    INSERT INTO question (pregunta,capitulo_id,calsificacion) VALUE("Respondo ante mi jefe por los resultados de toda mi área de trabajo",capitulo_id,1);/*11*/
-    INSERT INTO question (pregunta,capitulo_id,calsificacion) VALUE("En mi trabajo me dan órdenes contradictorias",capitulo_id,1);/*12*/
-    INSERT INTO question (pregunta,capitulo_id,calsificacion) VALUE("Considero que en mi trabajo me piden hacer cosas innecesarias",capitulo_id,1);/*13*/
+    INSERT INTO question (pregunta,capitulo_id,calsificacion,item) VALUE("En mi trabajo soy responsable de cosas de mucho valor",capitulo_id,1,10);/*10*/
+    INSERT INTO question (pregunta,capitulo_id,calsificacion,item) VALUE("Respondo ante mi jefe por los resultados de toda mi área de trabajo",capitulo_id,1,11);/*11*/
+    INSERT INTO question (pregunta,capitulo_id,calsificacion,item) VALUE("En mi trabajo me dan órdenes contradictorias",capitulo_id,1,12);/*12*/
+    INSERT INTO question (pregunta,capitulo_id,calsificacion,item) VALUE("Considero que en mi trabajo me piden hacer cosas innecesarias",capitulo_id,1,13);/*13*/
 END$$
 DELIMITER ;
 
@@ -163,10 +163,10 @@ DELIMITER $$
     DROP PROCEDURE IF EXISTS `preguntasCapitulo3` $$
     CREATE PROCEDURE `preguntasCapitulo3`(IN capitulo_id INT)
 BEGIN
-    INSERT INTO question (pregunta,capitulo_id,calsificacion) VALUE("Trabajo horas extras más de tres veces a la semana",capitulo_id,1);/*14*/
-    INSERT INTO question (pregunta,capitulo_id,calsificacion) VALUE("Mi trabajo me exige laborar en días de descanso, festivos o fines de semana",capitulo_id,1);/*15*/
-    INSERT INTO question (pregunta,capitulo_id,calsificacion) VALUE("Considero que el tiempo en el trabajo es mucho y perjudica mis actividades familiares o personales",capitulo_id,1);/*16*/
-    INSERT INTO question (pregunta,capitulo_id,calsificacion) VALUE("Pienso en las actividades familiares o personales cuando estoy en mi trabajo",capitulo_id,1);/*17*/
+    INSERT INTO question (pregunta,capitulo_id,calsificacion,item) VALUE("Trabajo horas extras más de tres veces a la semana",capitulo_id,1,14);/*14*/
+    INSERT INTO question (pregunta,capitulo_id,calsificacion,item) VALUE("Mi trabajo me exige laborar en días de descanso, festivos o fines de semana",capitulo_id,1,15);/*15*/
+    INSERT INTO question (pregunta,capitulo_id,calsificacion,item) VALUE("Considero que el tiempo en el trabajo es mucho y perjudica mis actividades familiares o personales",capitulo_id,1,16);/*16*/
+    INSERT INTO question (pregunta,capitulo_id,calsificacion,item) VALUE("Pienso en las actividades familiares o personales cuando estoy en mi trabajo",capitulo_id,1,17);/*17*/
 END$$
 DELIMITER ;
 
@@ -174,11 +174,11 @@ DELIMITER $$
     DROP PROCEDURE IF EXISTS `preguntasCapitulo4` $$
     CREATE PROCEDURE `preguntasCapitulo4`(IN capitulo_id INT)
 BEGIN
-    INSERT INTO question (pregunta,capitulo_id,calsificacion) VALUE("Mi trabajo permite que desarrolle nuevas habilidades",capitulo_id,2);/*18*/
-    INSERT INTO question (pregunta,capitulo_id,calsificacion) VALUE("En mi trabajo puedo aspirar a un mejor puesto",capitulo_id,2);/*19*/
-    INSERT INTO question (pregunta,capitulo_id,calsificacion) VALUE("Durante mi jornada de trabajo puedo tomar pausas cuando las necesito",capitulo_id,2);/*20*/
-    INSERT INTO question (pregunta,capitulo_id,calsificacion) VALUE("Puedo decidir cuánto trabajo realizo durante la jornada laboral",capitulo_id,2);/*21*/
-    INSERT INTO question (pregunta,capitulo_id,calsificacion) VALUE("Puedo decidir la velocidad a la que realizo mis actividades en mi trabajo",capitulo_id,2);/*22*/
+    INSERT INTO question (pregunta,capitulo_id,calsificacion,item) VALUE("Mi trabajo permite que desarrolle nuevas habilidades",capitulo_id,2,18);/*18*/
+    INSERT INTO question (pregunta,capitulo_id,calsificacion,item) VALUE("En mi trabajo puedo aspirar a un mejor puesto",capitulo_id,2,19);/*19*/
+    INSERT INTO question (pregunta,capitulo_id,calsificacion,item) VALUE("Durante mi jornada de trabajo puedo tomar pausas cuando las necesito",capitulo_id,2,20);/*20*/
+    INSERT INTO question (pregunta,capitulo_id,calsificacion,item) VALUE("Puedo decidir cuánto trabajo realizo durante la jornada laboral",capitulo_id,2,21);/*21*/
+    INSERT INTO question (pregunta,capitulo_id,calsificacion,item) VALUE("Puedo decidir la velocidad a la que realizo mis actividades en mi trabajo",capitulo_id,2,22);/*22*/
 
 END$$
 DELIMITER ;
@@ -187,11 +187,11 @@ DELIMITER $$
     DROP PROCEDURE IF EXISTS `preguntasCapitulo5` $$
     CREATE PROCEDURE `preguntasCapitulo5`(IN capitulo_id INT)
 BEGIN
-    INSERT INTO question (pregunta,capitulo_id,calsificacion) VALUE("Me informan con claridad cuáles son mis funciones",capitulo_id,2);/*23*/
-    INSERT INTO question (pregunta,capitulo_id,calsificacion) VALUE("Me explican claramente los resultados que debo obtener en mi trabajo",capitulo_id,2);/*24*/
-    INSERT INTO question (pregunta,capitulo_id,calsificacion) VALUE("Me informan con quién puedo resolver problemas o asuntos de trabajo",capitulo_id,2);/*25*/
-    INSERT INTO question (pregunta,capitulo_id,calsificacion) VALUE("Me permiten asistir a capacitaciones relacionadas con mi trabajo",capitulo_id,2);/*26*/
-    INSERT INTO question (pregunta,capitulo_id,calsificacion) VALUE("Recibo capacitación útil para hacer mi trabajo",capitulo_id,2);/*27*/
+    INSERT INTO question (pregunta,capitulo_id,calsificacion,item) VALUE("Me informan con claridad cuáles son mis funciones",capitulo_id,2,23);/*23*/
+    INSERT INTO question (pregunta,capitulo_id,calsificacion,item) VALUE("Me explican claramente los resultados que debo obtener en mi trabajo",capitulo_id,2,24);/*24*/
+    INSERT INTO question (pregunta,capitulo_id,calsificacion,item) VALUE("Me informan con quién puedo resolver problemas o asuntos de trabajo",capitulo_id,2,25);/*25*/
+    INSERT INTO question (pregunta,capitulo_id,calsificacion,item) VALUE("Me permiten asistir a capacitaciones relacionadas con mi trabajo",capitulo_id,2,26);/*26*/
+    INSERT INTO question (pregunta,capitulo_id,calsificacion,item) VALUE("Recibo capacitación útil para hacer mi trabajo",capitulo_id,2,27);/*27*/
 END$$
 DELIMITER ;
 
@@ -199,19 +199,19 @@ DELIMITER $$
     DROP PROCEDURE IF EXISTS `preguntasCapitulo6` $$
     CREATE PROCEDURE `preguntasCapitulo6`(IN capitulo_id INT)
 BEGIN
-    INSERT INTO question (pregunta,capitulo_id,calsificacion) VALUE("Mi jefe tiene en cuenta mis puntos de vista y opiniones",capitulo_id,2);/*28*/
-    INSERT INTO question (pregunta,capitulo_id,calsificacion) VALUE("Mi jefe ayuda a solucionar los problemas que se presentan en el trabajo",capitulo_id,2);/*29*/
-    INSERT INTO question (pregunta,capitulo_id,calsificacion) VALUE("Puedo confiar en mis compañeros de trabajo",capitulo_id,2);/*30*/
-    INSERT INTO question (pregunta,capitulo_id,calsificacion) VALUE("Cuando tenemos que realizar trabajo de equipo los compañeros colaboran",capitulo_id,2);/*31*/
-    INSERT INTO question (pregunta,capitulo_id,calsificacion) VALUE("Mis compañeros de trabajo me ayudan cuando tengo dificultades",capitulo_id,2);/*32*/
-    INSERT INTO question (pregunta,capitulo_id,calsificacion) VALUE("En mi trabajo puedo expresarme libremente sin interrupciones",capitulo_id,2);/*33*/
-    INSERT INTO question (pregunta,capitulo_id,calsificacion) VALUE("Recibo críticas constantes a mi persona y/o trabajo",capitulo_id,1);/*34*/
-    INSERT INTO question (pregunta,capitulo_id,calsificacion) VALUE("Recibo burlas, calumnias, difamaciones, humillaciones o ridiculizaciones",capitulo_id,1);/*35*/
-    INSERT INTO question (pregunta,capitulo_id,calsificacion) VALUE("Se ignora mi presencia o se me excluye de las reuniones de trabajo y en la toma de decisiones",capitulo_id,1);/*36*/
-    INSERT INTO question (pregunta,capitulo_id,calsificacion) VALUE("Se manipulan las situaciones de trabajo para hacerme parecer un mal trabajador",capitulo_id,1);/*37*/
-    INSERT INTO question (pregunta,capitulo_id,calsificacion) VALUE("Se ignoran mis éxitos laborales y se atribuyen a otros trabajadores",capitulo_id,1);/*38*/
-    INSERT INTO question (pregunta,capitulo_id,calsificacion) VALUE("Me bloquean o impiden las oportunidades que tengo para obtener ascenso o mejora en mi trabajo",capitulo_id,1);/*39*/
-    INSERT INTO question (pregunta,capitulo_id,calsificacion) VALUE("He presenciado actos de violencia en mi centro de trabajo",capitulo_id,1);/*40*/
+    INSERT INTO question (pregunta,capitulo_id,calsificacion,item) VALUE("Mi jefe tiene en cuenta mis puntos de vista y opiniones",capitulo_id,2,28);/*28*/
+    INSERT INTO question (pregunta,capitulo_id,calsificacion,item) VALUE("Mi jefe ayuda a solucionar los problemas que se presentan en el trabajo",capitulo_id,2,29);/*29*/
+    INSERT INTO question (pregunta,capitulo_id,calsificacion,item) VALUE("Puedo confiar en mis compañeros de trabajo",capitulo_id,2,30);/*30*/
+    INSERT INTO question (pregunta,capitulo_id,calsificacion,item) VALUE("Cuando tenemos que realizar trabajo de equipo los compañeros colaboran",capitulo_id,2,31);/*31*/
+    INSERT INTO question (pregunta,capitulo_id,calsificacion,item) VALUE("Mis compañeros de trabajo me ayudan cuando tengo dificultades",capitulo_id,2,32);/*32*/
+    INSERT INTO question (pregunta,capitulo_id,calsificacion,item) VALUE("En mi trabajo puedo expresarme libremente sin interrupciones",capitulo_id,2,33);/*33*/
+    INSERT INTO question (pregunta,capitulo_id,calsificacion,item) VALUE("Recibo críticas constantes a mi persona y/o trabajo",capitulo_id,1,34);/*34*/
+    INSERT INTO question (pregunta,capitulo_id,calsificacion,item) VALUE("Recibo burlas, calumnias, difamaciones, humillaciones o ridiculizaciones",capitulo_id,1,25);/*35*/
+    INSERT INTO question (pregunta,capitulo_id,calsificacion,item) VALUE("Se ignora mi presencia o se me excluye de las reuniones de trabajo y en la toma de decisiones",capitulo_id,1,36);/*36*/
+    INSERT INTO question (pregunta,capitulo_id,calsificacion,item) VALUE("Se manipulan las situaciones de trabajo para hacerme parecer un mal trabajador",capitulo_id,1,37);/*37*/
+    INSERT INTO question (pregunta,capitulo_id,calsificacion,item) VALUE("Se ignoran mis éxitos laborales y se atribuyen a otros trabajadores",capitulo_id,1,38);/*38*/
+    INSERT INTO question (pregunta,capitulo_id,calsificacion,item) VALUE("Me bloquean o impiden las oportunidades que tengo para obtener ascenso o mejora en mi trabajo",capitulo_id,1,39);/*39*/
+    INSERT INTO question (pregunta,capitulo_id,calsificacion,item) VALUE("He presenciado actos de violencia en mi centro de trabajo",capitulo_id,1,40);/*40*/
 END$$
 DELIMITER ;
 
@@ -219,9 +219,9 @@ DELIMITER $$
     DROP PROCEDURE IF EXISTS `preguntasCapitulo7` $$
     CREATE PROCEDURE `preguntasCapitulo7`(IN capitulo_id INT)
 BEGIN
-    INSERT INTO question (pregunta,capitulo_id,calsificacion) VALUE("Comunican tarde los asuntos del trabajo",capitulo_id,1);/*41*/
-    INSERT INTO question (pregunta,capitulo_id,calsificacion) VALUE("Dificultan el logro de los resultados del trabajo",capitulo_id,1);/*42*/
-    INSERT INTO question (pregunta,capitulo_id,calsificacion) VALUE("Ignoran las sugerencias para mejorar su trabajo",capitulo_id,1);/*43*/
+    INSERT INTO question (pregunta,capitulo_id,calsificacion,item) VALUE("Comunican tarde los asuntos del trabajo",capitulo_id,1,41);/*41*/
+    INSERT INTO question (pregunta,capitulo_id,calsificacion,item) VALUE("Dificultan el logro de los resultados del trabajo",capitulo_id,1,42);/*42*/
+    INSERT INTO question (pregunta,capitulo_id,calsificacion,item) VALUE("Ignoran las sugerencias para mejorar su trabajo",capitulo_id,1,43);/*43*/
 END$$
 DELIMITER ;
 
